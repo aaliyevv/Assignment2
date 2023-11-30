c22f9a8b63af2aefe44d51395284fb21873d2ab7
Ali Aliyev <alialiyev@Ali-MacBook-Pro-2.local>
2023-11-30 17:58:14 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+class ProductDisplay {
+    constructor(apiURL) {
+        this.apiURL = apiURL;
+        this.init();
+    }
+
+    init() {
+        document.addEventListener('DOMContentLoaded', () => this.fetchProducts());
+    }
+
+    async fetchProducts() {
+        try {
+            const response = await fetch(this.apiURL);
+            this.handleResponse(response);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    async handleResponse(response) {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        this.displayProducts(data.products);
+    }
+
+    displayProducts(products) {
+        const container = document.getElementById('products-container');
+        products.forEach(product => container.appendChild(this.createProductElement(product)));
+    }
+
+    createProductElement(product) {
+        const productElement = document.createElement('div');
+        productElement.className = 'product-card';
+        productElement.innerHTML = this.getProductHTML(product);
+        return productElement;
+    }
+
+    getProductHTML(product) {
+        return `
+            <div class="product-image">
+                <img src="${product.thumbnail}" alt="${product.title}" />
+            </div>
+            <div class="product-info">
+                <h2>${product.title}</h2>
+                <p>Price: $${product.price}</p>
+                <p>Discount: ${product.discountPercentage}%</p>
+                <p>Category: ${product.category}</p>
+                <p>Stock: ${product.stock}</p>
+            </div>
+        `;
+    }
+}
+
+new ProductDisplay('https://dummyjson.com/products');
